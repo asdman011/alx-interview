0e3645e8479a41d1283cb57b0efc155fcea1292d
Ammar Ashour <120216095+asdman011@users.noreply.github.com>
2024-09-13 00:29:02 +0300

    add

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,75 +1,44 @@
 #!/usr/bin/python3
+""" N queens """
 import sys
 
 
-def place_queens(size):
-    """Solves the N-Queens problem for a given board size.
-
-    Args:
-        size (int): The size of the chessboard and the number of
-                    queens to place.
-
-    Returns:
-        None: Prints each valid arrangement of queens on the chessboard.
-    """
-    occupied_columns = set()
-    occupied_rise_diagonals = set()
-    occupied_fall_diagonals = set()
-    positions = list()
-
-    def backtrack(row):
-        """Recursively attempts to place queens on the board.
-
-        This function places a queen in the current row and then
-        recursively attempts to place queens in the subsequent rows
-        (backtacks when needed).
-        If a valid configuration is found, it is printed.
-
-        Args:
-            row (int): The current row where the queen is to be placed.
-
-        Returns:
-            None
-        """
-        if row == size:
-            print(positions)
-            return
-
-        for col in range(size):
-            if (col not in occupied_columns and
-                (row + col) not in occupied_rise_diagonals and
-                    (row - col) not in occupied_fall_diagonals):
-
-                # Mark the current column and diagonals as occupied by Queen
-                occupied_columns.add(col)
-                occupied_rise_diagonals.add(row + col)
-                occupied_fall_diagonals.add(row - col)
-                positions.append([row, col])
-
-                # Recurse to place the next queen(in next row)
-                backtrack(row + 1)
-
-                # Backtrack: remove queen and reset occupied sets(wrong path)
-                occupied_columns.remove(col)
-                occupied_rise_diagonals.remove(row + col)
-                occupied_fall_diagonals.remove(row - col)
-                positions.pop()
-
-    backtrack(0)
-
-
-# Call the function
-if len(sys.argv) != 2:
+if len(sys.argv) > 2 or len(sys.argv) < 2:
     print("Usage: nqueens N")
-    sys.exit(1)
-else:
-    try:
-        n = int(sys.argv[1])
-        if n < 4:
-            print("N must be at least 4")
-            sys.exit(1)
-        else:
-            place_queens(int(sys.argv[1]))
-    except ValueError:
-        print("N must be a number")
-        sys.exit(1)
+    exit(1)
+
+if not sys.argv[1].isdigit():
+    print("N must be a number")
+    exit(1)
+
+if int(sys.argv[1]) < 4:
+    print("N must be at least 4")
+    exit(1)
+
+n = int(sys.argv[1])
+
+
+def queens(n, i=0, a=[], b=[], c=[]):
+    """ find possible positions """
+    if i < n:
+        for j in range(n):
+            if j not in a and i + j not in b and i - j not in c:
+                yield from queens(n, i + 1, a + [j], b + [i + j], c + [i - j])
+    else:
+        yield a
+
+
+def solve(n):
+    """ solve """
+    k = []
+    i = 0
+    for solution in queens(n, 0):
+        for s in solution:
+            k.append([i, s])
+            i += 1
+        print(k)
+        k = []
+        i = 0
+
+
+solve(n)
